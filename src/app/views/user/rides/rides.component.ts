f0a6f3cd883cf9df1bd6b4d2c3651fab972ed9f3
vishnuManimala <vishnumanimala8133@gmail.com>
2024-07-04 20:04:51 +0530

    rides

--- src/app/views/user/rides/rides.component.ts
+++ src/app/views/user/rides/rides.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RideSearchResponse } from 'src/app/models/ride.model';
+import { RideService } from 'src/app/services/ride.service';
+import { environment } from 'src/environments/environments';
 
 @Component({
   selector: 'app-rides',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./rides.component.css']
 })
 export class RidesComponent {
+  rideData: any;
+  picUrl: string = environment.picUrl;
+  
+  constructor(private _rideService: RideService, private _router: Router) { }
 
+  ngOnInit(): void {
+    this.getData(1);
+  }
+
+  getData(page: number) {
+    this._rideService.getRideData(page).subscribe((result: RideSearchResponse) => {
+      if (result.status === "Success") {
+        this.rideData = result.data;
+      }
+    })
+  }
+
+  navigator(id: string) {
+    this._router.navigate(['/user/profile'], { queryParams: { id: id } })
+  }
+
+  view(id: string) {
+    this._router.navigate(['/user/search/view'], { queryParams: { id: id } })
+  }
 }
